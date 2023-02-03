a9e36425f7342d7dc757844ce57b705a5e8b4fec
phhephhe <beridzedavit07@gmail.com>
2023-02-03 19:36:10 +0400

    added experience form

---
+++ javascripts/test.js
@@ -0,0 +1,6 @@
+// const inputField = document.getElementById("input-field");
+// const output = document.getElementById("output");
+
+// inputField.addEventListener("input", function() {
+//   output.textContent = inputField.value;
+// });
